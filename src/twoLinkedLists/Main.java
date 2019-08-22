acd6a4b325edbd8bcfcc139d3b43616a6bd46289
chaninkou <michaelkoulove@yahoo.com>
2019-08-21 23:56:00 -0700

    Not Done.

---
+++ src/twoLinkedLists/Main.java
@@ -0,0 +1,12 @@
+package twoLinkedLists;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] headA = {4,1,8,4,5};
+		int[] headB = {5,0,1,8,4,5};
+		
+		FindIntersectionOfTwoLinkedListFunction solution = new FindIntersectionOfTwoLinkedListFunction();
+		
+		
+	}
+}
